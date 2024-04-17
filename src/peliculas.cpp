c139e737ed1cfbfce84ab47805e16a1319c71260
BDG.Josue.Villagran <jvillagran@bdgsa.net>
2024-04-17 10:56:27 -0600

    CRUD Pelis

--- src/peliculas.cpp
+++ src/peliculas.cpp
@@ -98,8 +98,8 @@ void peliculas::desplegar()
 	system("cls");
 	fstream file;
 	int total=0;
-	cout<<"\n-------------------------Tabla de Detalles de Vendedores -------------------------"<<endl;
-	file.open("vendedores.txt",ios::in);
+	cout<<"\n-------------------------Tabla de Detalles de Peliculas -------------------------"<<endl;
+	file.open("peliculas.txt",ios::in);
 	if(!file)
 	{
 		cout<<"\n\t\t\tNo hay información...";
@@ -107,15 +107,21 @@ void peliculas::desplegar()
 	}
 	else
 	{
-		file >> id >> nombre >> telefono;
+		file >> id >> nombre >> clasificacion >> genero >> subtitulado >> idioma >> precio ;
 		while(!file.eof())
 		{
 			total++;
-			cout<<"\n\n\t\t\t Id Vendedor: "<<id<<endl;
-			cout<<"\t\t\t Nombre Vendedor: "<<nombre<<endl;
-            cout<<"\t\t\t Telefono Vendedor: "<<telefono<<endl;
+			cout<<"\n\n\t\t\t Id Pelicula   : "<<id<<endl;
+			cout<<"\t\t\t Nombre            : "<<nombre<<endl;
+            cout<<"\t\t\t Clasificacion     : "<<clasificacion<<endl;
+            cout<<"\t\t\t Genero            : "<<genero<<endl;
+            cout<<"\t\t\t Subtitulado       : "<<subtitulado<<endl;
+            cout<<"\t\t\t Idioma            : "<<idioma<<endl;
+            cout<<"\t\t\t Precio            : "<<precio<<endl;
 
-			file >> id >> nombre >> telefono;
+
+
+			file >> id >> nombre >> clasificacion >> genero >> subtitulado >> idioma >> precio ;
 		}
 		if(total==0)
 		{
@@ -124,8 +130,8 @@ void peliculas::desplegar()
 		system("pause");
 	}
 	file.close();
-    bitacora auditoria;
-    auditoria.insertar("usuario registrado", "701", "SEL");
+   // bitacora auditoria;
+    //auditoria.insertar("usuario registrado", "701", "SEL");
 }
 void peliculas::modificar()
 {
@@ -134,44 +140,60 @@ void peliculas::modificar()
 	string participant_id;
 	int found=0;
 	cout<<"\n-------------------------Modificacion Detalles Vendedor-------------------------"<<endl;
-	file.open("vendedores.txt",ios::in);
+	file.open("peliculas.txt",ios::in);
 	if(!file)
 	{
 		cout<<"\n\t\t\tNo hay informacion..,";
 		file.close();
+		system("pause");
 	}
 	else
 	{
-		cout<<"\n Ingrese Id del vendedor que quiere modificar: ";
+		cout<<"\n Ingrese Id de la pelicula que quiere modificar: ";
 		cin>>participant_id;
 		file1.open("temporal.txt",ios::app | ios::out);
-		file >> id >> nombre >> telefono;
+		file >> id >> nombre >> clasificacion >> genero >> subtitulado >> idioma >> precio ;
 		while(!file.eof())
 		{
 			if(participant_id!=id)
 			{
-			 file1<<std::left<<std::setw(15)<< id <<std::left<<std::setw(15)<< nombre <<std::left<<std::setw(15)<< telefono << "\n";
+			 file1<<std::left<<std::setw(15)<< id <<std::left<<std::setw(15)<< nombre <<std::left<<std::setw(15)<< clasificacion <<std::left<<std::setw(15)<< genero <<std::left<<std::setw(15)<< subtitulado <<std::left<<std::setw(15)<< idioma <<std::left<<std::setw(15)<< precio << "\n";
 			}
 			else
 			{
-				cout<<"\t\t\tIngrese Id Vendedor: ";
-				cin>>id;
-				cout<<"\t\t\tIngrese Nombre Vendedor: ";
-				cin>>nombre;
-				cout<<"\t\t\tIngrese Telefono Vendedor: ";
-				cin>>telefono;
-				file1<<std::left<<std::setw(15)<< id <<std::left<<std::setw(15)<< nombre <<std::left<<std::setw(15)<< telefono << "\n";
+					cout<<"\t\t\tIngresa Id             : ";
+                    cin>>id;
+                    cout<<"\t\t\tIngresa Nombre         : ";
+                    cin>>nombre;
+                    cout<<"\t\t\tIngresa Clasificacion  : ";
+                    cin>>clasificacion;
+                    cout<<"\t\t\tIngresa Genero         : ";
+                    cin>>genero;
+                    cout<<"\t\t\t¿Esta Subtitulada?     : ";
+                    cin>>subtitulado;
+                    cout<<"\t\t\tIdioma de la pelicula  : ";
+                    cin>>idioma;
+                    cout<<"\t\t\tIngresa Precio         : ";
+                    cin>>precio;
+
+				file1<<std::left<<std::setw(15)<< id <<std::left<<std::setw(15)<< nombre <<std::left<<std::setw(15)<< clasificacion <<std::left<<std::setw(15)<< genero <<std::left<<std::setw(15)<< subtitulado <<std::left<<std::setw(15)<< idioma <<std::left<<std::setw(15)<< precio << "\n";
 				found++;
 			}
-			file >> id >> nombre >> telefono;
-
+			file >> id >> nombre >> clasificacion >> genero >> subtitulado >> idioma >> precio ;
+			system("cls");
+            if (found>0)
+                cout << "Registro actualizado exitosamente!";
+            else
+                cout << "No se encontro el registro con el id ingresado!";
+            cout << endl << endl;
+            system("pause");
 		}
 		file1.close();
 		file.close();
-		remove("vendedores.txt");
-		rename("temporal.txt","vendedores.txt");
-        bitacora auditoria;
-        auditoria.insertar("usuario registrado", "701", "UPD");
+		remove("peliculas.txt");
+		rename("temporal.txt","peliculas.txt");
+        //bitacora auditoria;
+        //auditoria.insertar("usuario registrado", "701", "UPD");
 	}
 }
 void peliculas::buscar()
@@ -179,11 +201,12 @@ void peliculas::buscar()
 	system("cls");
 	fstream file;
 	int found=0;
-	file.open("vendedores.txt",ios::in);
+	file.open("peliculas.txt",ios::in);
 	if(!file)
 	{
 		cout<<"\n-------------------------Datos del vendedor buscado------------------------"<<endl;
-		cout<<"\n\t\t\tNo hay informacion...";
+		cout<<"\n\t\t\tNo hay informacion..."<< endl;
+		system("pause");
 	}
 	else
 	{
@@ -191,25 +214,29 @@ void peliculas::buscar()
 		cout<<"\n-------------------------Datos del vendedor buscado------------------------"<<endl;
 		cout<<"\nIngrese Id del vendedor que quiere buscar: ";
 		cin>>participant_id;
-		file >> id >> nombre >> telefono;
+		file >> id >> nombre >> clasificacion >> genero >> subtitulado >> idioma >> precio ;
 		while(!file.eof())
 		{
 			if(participant_id==id)
 			{
-				cout<<"\n\n\t\t\t Id Vendedor: "<<id<<endl;
-				cout<<"\t\t\t Nombre Vendedor: "<<nombre<<endl;
-				cout<<"\t\t\t Telefono Vendedor: "<<telefono<<endl;
-				found++;
+					cout<<"\n\n\t\t\t Id Pelicula   : "<<id<<endl;
+                    cout<<"\t\t\t Nombre            : "<<nombre<<endl;
+                    cout<<"\t\t\t Clasificacion     : "<<clasificacion<<endl;
+                    cout<<"\t\t\t Genero            : "<<genero<<endl;
+                    cout<<"\t\t\t Subtitulado       : "<<subtitulado<<endl;
+                    cout<<"\t\t\t Idioma            : "<<idioma<<endl;
+                    cout<<"\t\t\t Precio            : "<<precio<<endl;
+                    found++;
 			}
-			file >> id >> nombre >> telefono;
+        file >> id >> nombre >> clasificacion >> genero >> subtitulado >> idioma >> precio ;
 		}
 		if(found==0)
 		{
-			cout<<"\n\t\t\t Persona no encontrada...";
+			cout<<"\n\t\t\t Pelicula no encontrada..."<< endl  << endl ;
 		}
 		file.close();
-        bitacora auditoria;
-        auditoria.insertar("usuario registrado", "701", "SEL");
+		system("pause");
+
 	}
 }
 void peliculas::borrar()
@@ -219,40 +246,44 @@ void peliculas::borrar()
 	string participant_id;
 	int found=0;
 	cout<<"\n-------------------------Detalles Vendedor a Borrar-------------------------"<<endl;
-	file.open("vendedores.txt",ios::in);
+	file.open("peliculas.txt",ios::in);
 	if(!file)
 	{
-		cout<<"\n\t\t\tNo hay informacion...";
+		cout<<"\n\t\t\tNo hay informacion..." << endl ;
 		file.close();
+		system("pause");
 	}
 	else
 	{
-		cout<<"\n Ingrese el Id del vendedor que quiere borrar: ";
+		cout<<"\n Ingrese el Id de la pelicula que quiere borrar: ";
 		cin>>participant_id;
 		file1.open("temporal.txt",ios::app | ios::out);
-		file >> id >> nombre >> telefono;
+		file >> id >> nombre >> clasificacion >> genero >> subtitulado >> idioma >> precio ;
 		while(!file.eof())
 		{
 			if(participant_id!= id)
 			{
-				file1<<std::left<<std::setw(15)<< id <<std::left<<std::setw(15)<< nombre <<std::left<<std::setw(15)<< telefono << "\n";
+				file1<<std::left<<std::setw(15)<< id <<std::left<<std::setw(15)<< nombre <<std::left<<std::setw(15)<< clasificacion <<std::left<<std::setw(15)<< genero <<std::left<<std::setw(15)<< subtitulado <<std::left<<std::setw(15)<< idioma <<std::left<<std::setw(15)<< precio << "\n";
 			}
 			else
 			{
 				found++;
 				cout << "\n\t\t\tBorrado de informacion exitoso";
+
 			}
-			file >> id >> nombre >> telefono;
+			file >> id >> nombre >> clasificacion >> genero >> subtitulado >> idioma >> precio ;
 		}
 		if(found==0)
 		{
-			cout<<"\n\t\t\t Id Persona no encontrado...";
+			cout<<"\n\t\t\t Id no encontrado..." << endl << endl;
+
 		}
 		file1.close();
 		file.close();
-		remove("vendedores.txt");
-		rename("temporal.txt","vendedores.txt");
-        bitacora auditoria;
-        auditoria.insertar("usuario registrado", "701", "DEL");
+		remove("peliculas.txt");
+		rename("temporal.txt","peliculas.txt");
+		system("pause");
+        //bitacora auditoria;
+        //auditoria.insertar("usuario registrado", "701", "DEL");
 	}
 }
